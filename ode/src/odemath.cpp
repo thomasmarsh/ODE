8706451ea1956dea8ead9163c8efd1f36bf162d6
Russell Smith <russ@q12.org>
2001-04-22 00:18:11 +0000

    new dNormalize3() with bugfix

--- ode/src/odemath.cpp
+++ ode/src/odemath.cpp
@@ -23,6 +23,67 @@
 #include "ode/odemath.h"
 
 
+// this may be called for vectors `a' with extremely small magnitude, for
+// example the result of a cross product on two nearly perpendicular vectors.
+// we must be robust to these small vectors. to prevent numerical error,
+// first find the component a[i] with the largest magnitude and then scale
+// all the components by 1/a[i]. then we can compute the length of `a' and
+// scale the components by 1/l. this has been verified to work with vectors
+// containing the smallest representable numbers.
+
+void dNormalize3 (dVector3 a)
+{
+  dReal a0,a1,a2,aa0,aa1,aa2,l;
+  a0 = a[0];
+  a1 = a[1];
+  a2 = a[2];
+  dASSERT (a);
+  aa0 = dFabs(a0);
+  aa1 = dFabs(a1);
+  aa2 = dFabs(a2);
+  if (aa1 > aa0) {
+    if (aa2 > aa1) {
+      goto aa2_largest;
+    }
+    else {		// aa1 is largest
+      a0 /= aa1;
+      a2 /= aa1;
+      l = dRecipSqrt (a0*a0 + a2*a2 + 1);
+      a[0] = a0*l;
+      a[1] = copysign(l,a1);
+      a[2] = a2*l;
+    }
+  }
+  else {
+    if (aa2 > aa0) {
+      aa2_largest:	// aa2 is largest
+      a0 /= aa2;
+      a1 /= aa2;
+      l = dRecipSqrt (a0*a0 + a1*a1 + 1);
+      a[0] = a0*l;
+      a[1] = a1*l;
+      a[2] = copysign(l,a2);
+    }
+    else {		// aa0 is largest
+      if (aa0 <= 0) {
+	a[0] = 1;	// if all a's are zero, this is where we'll end up.
+	a[1] = 0;	// return a default unit length vector.
+	a[2] = 0;
+	return;
+      }
+      a1 /= aa0;
+      a2 /= aa0;
+      l = dRecipSqrt (a1*a1 + a2*a2 + 1);
+      a[0] = copysign(l,a0);
+      a[1] = a1*l;
+      a[2] = a2*l;
+    }
+  }
+}
+
+
+/* OLD VERSION */
+/*
 void dNormalize3 (dVector3 a)
 {
   dASSERT (a);
@@ -39,6 +100,7 @@ void dNormalize3 (dVector3 a)
     a[2] = 0;
   }
 }
+*/
 
 
 void dNormalize4 (dVector4 a)
