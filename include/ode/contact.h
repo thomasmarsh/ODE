9b6f985eadd195b8108f6a1dd1d998071521b4e6
Russell Smith <none@none>
2001-05-05 03:17:19 +0000

    comments moved to ode.doc
    --HG--
    branch : ode

--- include/ode/contact.h
+++ include/ode/contact.h
@@ -29,64 +29,48 @@ extern "C" {
 #endif
 
 
-/* contact mode flags: these flags say which parameters in dSurfaceParameters
- * structure are used. the friction directions 1 and 2 are perpendicular to
- * the normal, i.e. tangential to the contact surface. fdir2 is computed from
- * the fdir1 and the normal.
- * any number of these flags can be ORed together.
- * NOT implemented yet: bounce, soft, slip.
- */
-
 enum {
-  dContactMu2		= 0x001, /* 0=use mu for both f-directions,
-				    1=use mu for fdir1, mu2 for fdir2 */
-  dContactFDir1		= 0x002, /* 1=use `fdir1', else compute it */
-  dContactBounce	= 0x004, /* objects bounce off each other */
-  dContactSoft		= 0x008, /* objects can penetrate if forced. damped */
-  dContactMotion1	= 0x010, /* relative surface motion, f-direction 1 */
-  dContactMotion2	= 0x020, /* relative surface motion, f-direction 2 */
-  dContactSlip1		= 0x040, /* 1st order slip, f-direction 1 */
-  dContactSlip2		= 0x080, /* 1st order slip, f-direction 2 */
+  dContactMu2		= 0x001,
+  dContactFDir1		= 0x002,
+  dContactBounce	= 0x004,
+  dContactSoft		= 0x008,
+  dContactMotion1	= 0x010,
+  dContactMotion2	= 0x020,
+  dContactSlip1		= 0x040,
+  dContactSlip2		= 0x080
 };
 
 
-/* surface parameters structure.
- * mu=0 corresponds to frictionless.
- * mu=dInfinity is particularly efficient (no LCP switching)
- */
-
 struct dSurfaceParameters {
-  /* the following numbers must always be defined */
-  int mode;              /* contact flags */
-  dReal mu;              /* Coulomb friction coefficient (0..dInfinity) */
+  /* must always be defined */
+  int mode;
+  dReal mu;
 
-  /* the following numbers are only defined if the corresponding flag is set */
-  dReal mu2;             /* Coulomb friction coefficient (0..dInfinity) */
-  dReal bounce;	         /* restitution parameter (0..1) */
-  dReal bounceVel;       /* minimum velocity for restitution (m/s) */
-  dReal soft;            /* contact softness parameter (soft mode) (0..) */
-  dReal motion1,motion2; /* surface velocity in direction 1/2 (m/s) */
-  dReal slip1,slip2;     /* 1st order slip in direction 1/2 */
+  /* only defined if the corresponding flag is set in mode */
+  dReal mu2;
+  dReal bounce;
+  dReal bounceVel;
+  dReal soft;
+  dReal motion1,motion2;
+  dReal slip1,slip2;
 };
 
 
 /* contact info set by collision functions */
 
 struct dContactGeom {
-  dVector3 pos;		/* position of the contact in global coordinates */
-  dVector3 normal;	/* normal vector, points `in' to object 1, length=1 */
-  dReal depth;		/* penetration depth */
+  dVector3 pos;
+  dVector3 normal;
+  dReal depth;
 };
 
 
-/* contact info used by contact joint. if fdir1 is given it must be unit
- * length and perpendicular to the normal.
- */
+/* contact info used by contact joint */
 
 struct dContact {
-  dSurfaceParameters surface;	/* set by user */
-  dContactGeom geom;		/* set by collision functions */
-  dVector3 fdir1;		/* first tangential friction direction */
+  dSurfaceParameters surface;
+  dContactGeom geom;
+  dVector3 fdir1;
 };
 
 
