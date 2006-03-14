7f552766d1d28eeabf731d3f594b64d17b0666ed
Bram Stolk <b.stolk@gmail.com>
2006-03-14 19:59:38 +0000

    dReals are better than floats

--- ode/src/collision_quadtreespace.cpp
+++ ode/src/collision_quadtreespace.cpp
@@ -351,7 +351,7 @@ dxQuadTreeSpace::dxQuadTreeSpace(dSpaceID _space, dVector3 Center, dVector3 Exte
 
 	int BlockCount = 0;
 	for (int i = 0; i <= Depth; i++){
-		BlockCount += (int)pow((float)SPLITS, i);
+		BlockCount += (int)pow((dReal)SPLITS, i);
 	}
 
 	Blocks = (Block*)dAlloc(BlockCount * sizeof(Block));
@@ -384,7 +384,7 @@ dxQuadTreeSpace::~dxQuadTreeSpace(){
 
 	int BlockCount = 0;
 	for (int i = 0; i < Depth; i++){
-		BlockCount += (int)pow((float)SPLITS, i);
+		BlockCount += (int)pow((dReal)SPLITS, i);
 	}
 
 	dFree(Blocks, BlockCount * sizeof(Block));
