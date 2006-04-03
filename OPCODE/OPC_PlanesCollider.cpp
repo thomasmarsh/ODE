c3d0806f59ec08bb7ea90da2ea27dad7e12f62c2
Tanguy Fautre <none@none>
2006-04-03 10:38:23 +0000

    - fixed compilation on x86-64 (ambiguous overloads: conversion from size_t to udword must be explicit) - linking is still broken (-fPIC problem)

--- OPCODE/OPC_PlanesCollider.cpp
+++ OPCODE/OPC_PlanesCollider.cpp
@@ -38,7 +38,7 @@ using namespace Opcode;
 #define SET_CONTACT(prim_index, flag)		\
 	/* Set contact status */				\
 	mFlags |= flag;							\
-	mTouchedPrimitives->Add(prim_index);
+	mTouchedPrimitives->Add(udword(prim_index));
 
 //! Planes-triangle test
 #define PLANES_PRIM(prim_index, flag)		\
