8bd4898e43df0ca707bdd664b6b55965b24cd517
Bram Stolk <b.stolk@gmail.com>
2006-04-12 07:47:47 +0000

    accept a path to textures

--- ode/test/test_motor.cpp
+++ ode/test/test_motor.cpp
@@ -141,6 +141,10 @@ int main (int argc, char **argv)
   fn.command = &command;
   fn.stop = 0;
   fn.path_to_textures = "../../drawstuff/textures";
+  if(argc>=2)
+  {
+    fn.path_to_textures = argv[1];
+  }
 
   // create world
   contactgroup = dJointGroupCreate(0);
