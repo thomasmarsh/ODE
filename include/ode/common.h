b0ba9e7d65d81003a05b476b6746dc8432f5ac25
Bram Stolk <none@none>
2006-04-23 14:48:47 +0000

    Add NAN support

--- include/ode/common.h
+++ include/ode/common.h
@@ -130,6 +130,7 @@ typedef dReal dQuaternion[4];
 #define dFabs(x) (fabsf(x))			/* absolute value */
 #define dAtan2(y,x) (atan2f(y,x))		/* arc tangent with 2 args */
 #define dFMod(a,b) (fmodf(a,b))		/* modulo */
+#define dIsNan(x) (isnanf(x))
 #define dCopySign(a,b) ((dReal)copysignf(a,b))
 
 #elif defined(dDOUBLE)
@@ -143,6 +144,7 @@ typedef dReal dQuaternion[4];
 #define dFabs(x) fabs(x)
 #define dAtan2(y,x) atan2((y),(x))
 #define dFMod(a,b) (fmod((a),(b)))
+#define dIsNan(x) (isnan(x))
 #define dCopySign(a,b) (copysign((a),(b)))
 
 #else
