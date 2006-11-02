8e5d50a922207781936ff7aaa20f1173db2ecd2a
Jason Perkins <jason@379.com>
2006-11-02 18:23:53 +0000

    Another big chunk of the Ode.NET binding

--- contrib/Ode.NET/Ode/premake.lua
+++ contrib/Ode.NET/Ode/premake.lua
@@ -2,17 +2,30 @@ package.name = "Ode.NET"
 package.kind = "dll"
 package.language = "c#"
 
-if (options["with-doubles"]) then
-  package.defines = { "dDOUBLE" }
-else
-  package.defines = { "dSINGLE " }
-end
+-- Build options
 
-package.links = {
-  "System"
-}
+  package.defines = { }
 
-package.files = {
-  "AssemblyInfo.cs",
-  "Ode.cs"
-}
+  if (options["with-doubles"]) then
+    table.insert(package.defines, "dDOUBLE")
+  else
+    table.insert(package.defines, "dSINGLE")
+  end
+
+  if (options["no-unsafe"]) then
+    table.insert(package.defines, "dNO_UNSAFE_CODE")
+  else
+    package.buildflags = { "unsafe" }
+  end
+  
+
+-- Files & Libraries
+
+  package.files = {
+    "AssemblyInfo.cs",
+    "Ode.cs"
+  }
+
+  package.links = {
+    "System"
+  }
