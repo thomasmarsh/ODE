db9c6bcd34e6779be0cd8eaee132df67ae4d5c41
David Walters <hidden.asbestos@googlemail.com>
2007-11-11 00:38:24 +0000

    Changed the 16 bit index build option to dTRIMESH_16BIT_INDICES. This will let us cleanly add GIMPACT support for this feature at a later date.

--- include/ode/common.h
+++ include/ode/common.h
@@ -115,16 +115,17 @@ typedef double dReal;
 #endif
 #endif // dTRIMESH_ENABLED
 
+// Define a type for indices, either 16 or 32 bit, based on build option
+// TODO: Currently GIMPACT only supports 32 bit indices.
+#if dTRIMESH_16BIT_INDICES
 #if dTRIMESH_GIMPACT
 typedef uint32 dTriIndex;
 #else // dTRIMESH_GIMPACT
-// Define a type for OPCODE indices, either 16 or 32 bit, based on build option
-#if dTRIMESH_OPCODE_16BIT
 typedef uint16 dTriIndex;
-#else // dTRIMESH_OPCODE_16BIT
-typedef uint32 dTriIndex;
-#endif // dTRIMESH_OPCODE_16BIT
 #endif // dTRIMESH_GIMPACT
+#else // dTRIMESH_16BIT_INDICES
+typedef uint32 dTriIndex;
+#endif // dTRIMESH_16BIT_INDICES
 
 /* round an integer up to a multiple of 4, except that 0 and 1 are unmodified
  * (used to compute matrix leading dimensions)
