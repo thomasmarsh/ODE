9928616860fbbc0788e5a70774897442647ff68b
David Walters <hidden.asbestos@googlemail.com>
2008-01-02 22:23:35 +0000

    Fixed Visual Studio errors regarding basic numerical type definitions ( unsigned __int8 instead of __uint8 ); Fixed unrelated double cast warning on default linear and angular damping thresholds.

--- ode/src/common-internal.h
+++ ode/src/common-internal.h
@@ -20,12 +20,12 @@
 #include <inttypes.h>
 #else
 #ifdef _MSC_VER
-  typedef __int32         int32_t;
-  typedef __uint32        uint32_t;
-  typedef __int16         int16_t;
-  typedef __uint16        uint16_t;
-  typedef __int8          int8_t;
-  typedef __uint8         uint8_t;
+  typedef __int32           int32_t;
+  typedef unsigned __int32  uint32_t;
+  typedef __int16           int16_t;
+  typedef unsigned __int16  uint16_t;
+  typedef __int8            int8_t;
+  typedef unsigned __int8   uint8_t;
 #else
   typedef int             int32_t;
   typedef unsigned        uint32_t;
