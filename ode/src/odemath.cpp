bb546ac986cc998a19a64837a89ec6094938f1c6
David Walters <none@none>
2008-03-14 14:10:27 +0000

    [ 1655333 ] Optimize the function dNormalize3

--- ode/src/odemath.cpp
+++ ode/src/odemath.cpp
@@ -46,52 +46,47 @@
 
 int dSafeNormalize3 (dVector3 a)
 {
-  dReal a0,a1,a2,aa0,aa1,aa2,l;
   dAASSERT (a);
-  a0 = a[0];
-  a1 = a[1];
-  a2 = a[2];
-  aa0 = dFabs(a0);
-  aa1 = dFabs(a1);
-  aa2 = dFabs(a2);
-  if (aa1 > aa0) {
-    if (aa2 > aa1) {
-      goto aa2_largest;
+
+  int idx;
+  dReal aa[3], l;
+
+  aa[0] = dFabs(a[0]);
+  aa[1] = dFabs(a[1]);
+  aa[2] = dFabs(a[2]);
+  if (aa[1] > aa[0]) {
+    if (aa[2] > aa[1]) { // aa[2] is largest
+      idx = 2;
     }
-    else {		// aa1 is largest
-      a0 /= aa1;
-      a2 /= aa1;
-      l = dRecipSqrt (a0*a0 + a2*a2 + 1);
-      a[0] = a0*l;
-      a[1] = dCopySign(l,a1);
-      a[2] = a2*l;
+    else {              // aa[1] is largest
+      idx = 1;
     }
   }
   else {
-    if (aa2 > aa0) {
-      aa2_largest:	// aa2 is largest
-      a0 /= aa2;
-      a1 /= aa2;
-      l = dRecipSqrt (a0*a0 + a1*a1 + 1);
-      a[0] = a0*l;
-      a[1] = a1*l;
-      a[2] = dCopySign(l,a2);
+    if (aa[2] > aa[0]) {// aa[2] is largest
+      idx = 2;
     }
-    else {		// aa0 is largest
-      if (aa0 <= 0) {
+    else {              // aa[0] might be the largest
+      if (aa[0] <= 0) { // aa[0] might is largest
 	a[0] = 1;	// if all a's are zero, this is where we'll end up.
 	a[1] = 0;	// return a default unit length vector.
 	a[2] = 0;
 	return 0;
       }
-      a1 /= aa0;
-      a2 /= aa0;
-      l = dRecipSqrt (a1*a1 + a2*a2 + 1);
-      a[0] = dCopySign(l,a0);
-      a[1] = a1*l;
-      a[2] = a2*l;
+      else {
+        idx = 0;
+      }
     }
   }
+
+  a[0] /= aa[idx];
+  a[1] /= aa[idx];
+  a[2] /= aa[idx];
+  l = dRecipSqrt (a[0]*a[0] + a[1]*a[1] + a[2]*a[2]);
+  a[0] *= l;
+  a[1] *= l;
+  a[2] *= l;
+  
   return 1;
 }
 
