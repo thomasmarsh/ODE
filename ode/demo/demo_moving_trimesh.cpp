b6069c5ffeef5fd51959056f1688a59267c8442f
Daniel K. O. <none@none>
2008-03-31 22:27:21 +0000

    Fixed bug in demo_moving_trimesh: don't use dReal, data is always stored in floats

--- ode/demo/demo_moving_trimesh.cpp
+++ ode/demo/demo_moving_trimesh.cpp
@@ -551,9 +551,9 @@ int main (int argc, char **argv)
 
   // note: can't share tridata if intending to trimesh-trimesh collide
   TriData1 = dGeomTriMeshDataCreate();
-  dGeomTriMeshDataBuildSingle(TriData1, &Vertices[0], 3 * sizeof(dReal), VertexCount, (dTriIndex*)&Indices[0], IndexCount, 3 * sizeof(dTriIndex));
+  dGeomTriMeshDataBuildSingle(TriData1, &Vertices[0], 3 * sizeof(float), VertexCount, (dTriIndex*)&Indices[0], IndexCount, 3 * sizeof(dTriIndex));
   TriData2 = dGeomTriMeshDataCreate();
-  dGeomTriMeshDataBuildSingle(TriData2, &Vertices[0], 3 * sizeof(dReal), VertexCount, (dTriIndex*)&Indices[0], IndexCount, 3 * sizeof(dTriIndex));
+  dGeomTriMeshDataBuildSingle(TriData2, &Vertices[0], 3 * sizeof(float), VertexCount, (dTriIndex*)&Indices[0], IndexCount, 3 * sizeof(dTriIndex));
   
   TriMesh1 = dCreateTriMesh(space, TriData1, 0, 0, 0);
   TriMesh2 = dCreateTriMesh(space, TriData2, 0, 0, 0);
