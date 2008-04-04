ce377af9fc2262d420e882482e4b1031600d1c10
Remi Ricard <remi.ricard@simlog.com>
2008-04-04 18:38:04 +0000

    - Add the properties svn:eol-style 'native' to odeconfig.h - Remove duplicate call to recomputeAABB introduced by the commit 1367

--- ode/src/collision_trimesh_box.cpp
+++ ode/src/collision_trimesh_box.cpp
@@ -1299,8 +1299,6 @@ int dCollideBTL(dxGeom* g1, dxGeom* BoxGeom, int Flags, dContactGeom* Contacts, 
 
   
   dxTriMesh* TriMesh = (dxTriMesh*)g1;
-  g1 -> recomputeAABB();
-  BoxGeom -> recomputeAABB();
 
   g1 -> recomputeAABB();
   BoxGeom -> recomputeAABB();
