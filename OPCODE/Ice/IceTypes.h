637c4397cdf9b5b50629cca516d796d10ed18e69
Oleh Derevenko <oleh.derevenko@gmail.com>
2008-07-19 11:42:57 +0000

    Fixed: Unsafe and unnecessary redefinition of bool type on MacOS has been removed.

--- OPCODE/Ice/IceTypes.h
+++ OPCODE/Ice/IceTypes.h
@@ -15,15 +15,6 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Things to help us compile on non-windows platforms
 
-#if defined(__MACOSX__) || defined(__APPLE__)
-#undef bool
-#define bool char
-#undef true
-#define true ((bool)-1)
-#undef false
-#define false ((bool)0)
-#endif // mac stuff
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	#define USE_HANDLE_MANAGER
@@ -68,7 +59,6 @@
 	typedef float				float32;	//!< sizeof(float32)	must be 4
 	typedef double				float64;	//!< sizeof(float64)	must be 4
 
-	ICE_COMPILE_TIME_ASSERT(sizeof(bool)==1);	// ...otherwise things might fail with VC++ 4.2 !
 	ICE_COMPILE_TIME_ASSERT(sizeof(ubyte)==1);
 	ICE_COMPILE_TIME_ASSERT(sizeof(sbyte)==1);
 	ICE_COMPILE_TIME_ASSERT(sizeof(sword)==2);
