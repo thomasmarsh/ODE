5f04509ffa106041dcdc9221c88cefb56922723c
Daniel K. O. <none@none>
2008-08-07 21:14:19 +0000

    removed static functions, to fix static build issues with drawstuff

--- ode/src/mass.cpp
+++ ode/src/mass.cpp
@@ -539,3 +539,16 @@ void dMassAdd (dMass *a, const dMass *b)
   a->mass += b->mass;
   for (i=0; i<12; i++) a->I[i] += b->I[i];
 }
+
+
+// Backwards compatible API
+void dMassSetCappedCylinder(dMass *a, dReal b, int c, dReal d, dReal e)
+{
+  return dMassSetCapsule(a,b,c,d,e);
+}
+
+void dMassSetCappedCylinderTotal(dMass *a, dReal b, int c, dReal d, dReal e)
+{
+  return dMassSetCapsuleTotal(a,b,c,d,e);
+}
+
