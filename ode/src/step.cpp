2e48e6bfe41b168329b27e0c9d5ecaf33a6b3fe5
Oleh Derevenko <oleh.derevenko@gmail.com>
2009-10-25 17:44:06 +0000

    Cosmetic: Dead code removed in step.cpp and lcp.cpp

--- ode/src/step.cpp
+++ ode/src/step.cpp
@@ -38,21 +38,6 @@
 //#define TIMING
 
 
-//****************************************************************************
-// debugging - comparison of various vectors and matrices produced by the
-// slow and fast versions of the stepper.
-
-//#define COMPARE_METHODS
-
-#ifdef COMPARE_METHODS
-#include "testing.h"
-static dMatrixComparison comparator;
-#endif
-
-// undef to use the fast decomposition
-#define DIRECT_CHOLESKY
-#undef REPORT_ERROR
-
 #ifdef TIMING
 #define IFTIMING(x) x
 #else
@@ -195,322 +180,6 @@ static void Multiply1_8q1 (dReal *A, const dReal *B, const dReal *C, int q)
   A[5] = sum5;
   A[6] = sum6;
 }
-/*
-//****************************************************************************
-// the slow, but sure way
-// note that this does not do any joint feedback!
-
-// given lists of bodies and joints that form an island, perform a first
-// order timestep.
-//
-// `body' is the body array, `nb' is the size of the array.
-// `_joint' is the body array, `nj' is the size of the array.
-
-void dInternalStepIsland_x1 (dxWorld *world, dxBody * const *body, int nb,
-                             dxJoint * const *_joint, int nj, dReal stepsize)
-{
-  int i,j,k;
-  int n6 = 6*nb;
-
-  IFTIMING(dTimerStart("preprocessing"));
-
-  // number all bodies in the body list - set their tag values
-  for (i=0; i<nb; i++) body[i]->tag = i;
-
-  // make a local copy of the joint array, because we might want to modify it.
-  // (the "dxJoint *const*" declaration says we're allowed to modify the joints
-  // but not the joint array, because the caller might need it unchanged).
-  ALLOCA(dxJoint*,joint,nj*sizeof(dxJoint*));
-  memcpy (joint,_joint,nj * sizeof(dxJoint*));
-
-  // for all bodies, compute the inertia tensor and its inverse in the global
-  // frame, and compute the rotational force and add it to the torque
-  // accumulator.
-  // @@@ check computation of rotational force.
-  ALLOCA(dReal,I,3*nb*4*sizeof(dReal));
-  ALLOCA(dReal,invI,3*nb*4*sizeof(dReal));
-
-  //dSetZero (I,3*nb*4);
-  //dSetZero (invI,3*nb*4);
-  for (i=0; i<nb; i++) {
-    dReal tmp[12];
-    // compute inertia tensor in global frame
-    dMultiply2_333 (tmp,body[i]->mass.I,body[i]->posr.R);
-    dMultiply0_333 (I+i*12,body[i]->posr.R,tmp);
-    // compute inverse inertia tensor in global frame
-    dMultiply2_333 (tmp,body[i]->invI,body[i]->posr.R);
-    dMultiply0_333 (invI+i*12,body[i]->posr.R,tmp);
-    // compute rotational force
-    dMultiply0_331 (tmp,I+i*12,body[i]->avel);
-    dSubtractVectorCross3(body[i]->tacc,body[i]->avel,tmp);
-  }
-
-  // add the gravity force to all bodies
-  for (i=0; i<nb; i++) {
-    if ((body[i]->flags & dxBodyNoGravity)==0) {
-      body[i]->facc[0] += body[i]->mass.mass * world->gravity[0];
-      body[i]->facc[1] += body[i]->mass.mass * world->gravity[1];
-      body[i]->facc[2] += body[i]->mass.mass * world->gravity[2];
-    }
-  }
-
-  // get m = total constraint dimension, nub = number of unbounded variables.
-  // create constraint offset array and number-of-rows array for all joints.
-  // the constraints are re-ordered as follows: the purely unbounded
-  // constraints, the mixed unbounded + LCP constraints, and last the purely
-  // LCP constraints.
-  //
-  // joints with m=0 are inactive and are removed from the joints array
-  // entirely, so that the code that follows does not consider them.
-  int m = 0;
-  ALLOCA(dxJoint::Info1,info,nj*sizeof(dxJoint::Info1));
-  ALLOCA(int,ofs,nj*sizeof(int));
-
-  for (i=0, j=0; j<nj; j++) {	// i=dest, j=src
-    joint[j]->getInfo1 (info+i);
-    dIASSERT (info[i].m >= 0 && info[i].m <= 6 &&
-      info[i].nub >= 0 && info[i].nub <= info[i].m);
-    if (info[i].m > 0) {
-      joint[i] = joint[j];
-      i++;
-    }
-  }
-  nj = i;
-
-  // the purely unbounded constraints
-  for (i=0; i<nj; i++) if (info[i].nub == info[i].m) {
-    ofs[i] = m;
-    m += info[i].m;
-  }
-  //int nub = m;
-  // the mixed unbounded + LCP constraints
-  for (i=0; i<nj; i++) if (info[i].nub > 0 && info[i].nub < info[i].m) {
-    ofs[i] = m;
-    m += info[i].m;
-  }
-  // the purely LCP constraints
-  for (i=0; i<nj; i++) if (info[i].nub == 0) {
-    ofs[i] = m;
-    m += info[i].m;
-  }
-
-  // create (6*nb,6*nb) inverse mass matrix `invM', and fill it with mass
-  // parameters
-  IFTIMING(dTimerNow ("create mass matrix"));
-
-  int nskip = dPAD (n6);
-  ALLOCA(dReal, invM, n6*nskip*sizeof(dReal));
-
-  dSetZero (invM,n6*nskip);
-  for (i=0; i<nb; i++) {
-    dReal *MM = invM+(i*6)*nskip+(i*6);
-    MM[0] = body[i]->invMass;
-    MM[nskip+1] = body[i]->invMass;
-    MM[2*nskip+2] = body[i]->invMass;
-    MM += 3*nskip+3;
-    for (j=0; j<3; j++) for (k=0; k<3; k++) {
-      MM[j*nskip+k] = invI[i*12+j*4+k];
-    }
-  }
-
-  // assemble some body vectors: fe = external forces, v = velocities
-  ALLOCA(dReal,fe,n6*sizeof(dReal));
-  ALLOCA(dReal,v,n6*sizeof(dReal));
-
-  //dSetZero (fe,n6);
-  //dSetZero (v,n6);
-  for (i=0; i<nb; i++) {
-    for (j=0; j<3; j++) fe[i*6+j] = body[i]->facc[j];
-    for (j=0; j<3; j++) fe[i*6+3+j] = body[i]->tacc[j];
-    for (j=0; j<3; j++) v[i*6+j] = body[i]->lvel[j];
-    for (j=0; j<3; j++) v[i*6+3+j] = body[i]->avel[j];
-  }
-
-  // this will be set to the velocity update
-  ALLOCA(dReal,vnew,n6*sizeof(dReal));
-  dSetZero (vnew,n6);
-
-  // if there are constraints, compute cforce
-  if (m > 0) {
-    // create a constraint equation right hand side vector `c', a constraint
-    // force mixing vector `cfm', and LCP low and high bound vectors, and an
-    // 'findex' vector.
-    ALLOCA(dReal,c,m*sizeof(dReal));
-    ALLOCA(dReal,cfm,m*sizeof(dReal));
-    ALLOCA(dReal,lo,m*sizeof(dReal));
-    ALLOCA(dReal,hi,m*sizeof(dReal));
-    ALLOCA(int,findex,m*sizeof(int));
-    dSetZero (c,m);
-    dSetValue (cfm,m,world->global_cfm);
-    dSetValue (lo,m,-dInfinity);
-    dSetValue (hi,m, dInfinity);
-    for (i=0; i<m; i++) findex[i] = -1;
-
-    // create (m,6*nb) jacobian mass matrix `J', and fill it with constraint
-    // data. also fill the c vector.
-    IFTIMING(dTimerNow ("create J"));
-    ALLOCA(dReal,J,m*nskip*sizeof(dReal));
-    dSetZero (J,m*nskip);
-    dxJoint::Info2 Jinfo;
-    Jinfo.rowskip = nskip;
-    Jinfo.fps = dRecip(stepsize);
-    Jinfo.erp = world->global_erp;
-    for (i=0; i<nj; i++) {
-      Jinfo.J1l = J + nskip*ofs[i] + 6*joint[i]->node[0].body->tag;
-      Jinfo.J1a = Jinfo.J1l + 3;
-      if (joint[i]->node[1].body) {
-        Jinfo.J2l = J + nskip*ofs[i] + 6*joint[i]->node[1].body->tag;
-        Jinfo.J2a = Jinfo.J2l + 3;
-      }
-      else {
-        Jinfo.J2l = 0;
-        Jinfo.J2a = 0;
-      }
-      Jinfo.c = c + ofs[i];
-      Jinfo.cfm = cfm + ofs[i];
-      Jinfo.lo = lo + ofs[i];
-      Jinfo.hi = hi + ofs[i];
-      Jinfo.findex = findex + ofs[i];
-      joint[i]->getInfo2 (&Jinfo);
-      // adjust returned findex values for global index numbering
-      for (j=0; j<info[i].m; j++) {
-        if (findex[ofs[i] + j] >= 0) findex[ofs[i] + j] += ofs[i];
-      }
-    }
-
-    // compute A = J*invM*J'
-    IFTIMING(dTimerNow ("compute A"));
-    ALLOCA(dReal,JinvM,m*nskip*sizeof(dReal));
-    //dSetZero (JinvM,m*nskip);
-    dMultiply0 (JinvM,J,invM,m,n6,n6);
-    int mskip = dPAD(m);
-    ALLOCA(dReal,A,m*mskip*sizeof(dReal));
-    //dSetZero (A,m*mskip);
-    dMultiply2 (A,JinvM,J,m,n6,m);
-
-    // add cfm to the diagonal of A
-    for (i=0; i<m; i++) A[i*mskip+i] += cfm[i] * Jinfo.fps;
-
-#   ifdef COMPARE_METHODS
-    comparator.nextMatrix (A,m,m,1,"A");
-#   endif
-
-    // compute `rhs', the right hand side of the equation J*a=c
-    IFTIMING(dTimerNow ("compute rhs"));
-    ALLOCA(dReal,tmp1,n6*sizeof(dReal));
-    //dSetZero (tmp1,n6);
-    dMultiply0 (tmp1,invM,fe,n6,n6,1);
-    for (i=0; i<n6; i++) tmp1[i] += v[i]/stepsize;
-    ALLOCA(dReal,rhs,m*sizeof(dReal));
-    //dSetZero (rhs,m);
-    dMultiply0 (rhs,J,tmp1,m,n6,1);
-    for (i=0; i<m; i++) rhs[i] = c[i]/stepsize - rhs[i];
-
-#   ifdef COMPARE_METHODS
-    comparator.nextMatrix (c,m,1,0,"c");
-    comparator.nextMatrix (rhs,m,1,0,"rhs");
-#   endif
-
-
-
-
-
-#ifndef DIRECT_CHOLESKY
-    // solve the LCP problem and get lambda.
-    // this will destroy A but that's okay
-    IFTIMING(dTimerNow ("solving LCP problem"));
-    ALLOCA(dReal,lambda,m*sizeof(dReal));
-    dSolveLCP (m,A,lambda,rhs,NULL,nub,lo,hi,findex);
-
-#ifdef dUSE_MALLOC_FOR_ALLOCA
-    if (dMemoryFlag == d_MEMORY_OUT_OF_MEMORY)
-      return;
-#endif
-
-
-#else
-
-    // OLD WAY - direct factor and solve
-
-    // factorize A (L*L'=A)
-    IFTIMING(dTimerNow ("factorize A"));
-    ALLOCA(dReal,L,m*mskip*sizeof(dReal));
-    memcpy (L,A,m*mskip*sizeof(dReal));
-    if (dFactorCholesky (L,m)==0) dDebug (0,"A is not positive definite");
-
-    // compute lambda
-    IFTIMING(dTimerNow ("compute lambda"));
-    ALLOCA(dReal,lambda,m*sizeof(dReal));
-    memcpy (lambda,rhs,m * sizeof(dReal));
-    dSolveCholesky (L,lambda,m);
-#endif
-
-#   ifdef COMPARE_METHODS
-    comparator.nextMatrix (lambda,m,1,0,"lambda");
-#   endif
-
-    // compute the velocity update `vnew'
-    IFTIMING(dTimerNow ("compute velocity update"));
-    dMultiply1 (tmp1,J,lambda,n6,m,1);
-    for (i=0; i<n6; i++) tmp1[i] += fe[i];
-    dMultiply0 (vnew,invM,tmp1,n6,n6,1);
-    for (i=0; i<n6; i++) vnew[i] = v[i] + stepsize*vnew[i];
-
-#ifdef REPORT_ERROR
-    // see if the constraint has worked: compute J*vnew and make sure it equals
-    // `c' (to within a certain tolerance).
-    IFTIMING(dTimerNow ("verify constraint equation"));
-    dMultiply0 (tmp1,J,vnew,m,n6,1);
-    dReal err = 0;
-    for (i=0; i<m; i++) {
-      err += dFabs(tmp1[i]-c[i]);
-    }
-    printf ("total constraint error=%.6e\n",err);
-#endif
-
-  }
-  else {
-    // no constraints
-    dMultiply0 (vnew,invM,fe,n6,n6,1);
-    for (i=0; i<n6; i++) vnew[i] = v[i] + stepsize*vnew[i];
-  }
-
-#ifdef COMPARE_METHODS
-  comparator.nextMatrix (vnew,n6,1,0,"vnew");
-#endif
-
-  // apply the velocity update to the bodies
-  IFTIMING(dTimerNow ("update velocity"));
-  for (i=0; i<nb; i++) {
-    for (j=0; j<3; j++) body[i]->lvel[j] = vnew[i*6+j];
-    for (j=0; j<3; j++) body[i]->avel[j] = vnew[i*6+3+j];
-  }
-
-  // update the position and orientation from the new linear/angular velocity
-  // (over the given timestep)
-  IFTIMING(dTimerNow ("update position"));
-  for (i=0; i<nb; i++) dxStepBody (body[i],stepsize);
-
-  IFTIMING(dTimerNow ("tidy up"));
-
-  // zero all force accumulators
-  for (i=0; i<nb; i++) {
-    body[i]->facc[0] = 0;
-    body[i]->facc[1] = 0;
-    body[i]->facc[2] = 0;
-    body[i]->facc[3] = 0;
-    body[i]->tacc[0] = 0;
-    body[i]->tacc[1] = 0;
-    body[i]->tacc[2] = 0;
-    body[i]->tacc[3] = 0;
-  }
-
-  IFTIMING(dTimerEnd());
-  if (m > 0) IFTIMING(dTimerReport (stdout,1));
-
-}
-*/
-
 
 //****************************************************************************
 // an optimized version of dInternalStepIsland1()
@@ -990,10 +659,6 @@ void dInternalStepIsland_x2 (dxWorldProcessContext *context,
 
     } END_STATE_SAVE(context, cfmstate);
 
-#   ifdef COMPARE_METHODS
-    comparator.nextMatrix (A,m,m,1,"A");
-#   endif
-
     BEGIN_STATE_SAVE(context, tmp1state) {
       // compute the right hand side `rhs'
       IFTIMING(dTimerNow ("compute rhs"));
@@ -1013,10 +678,6 @@ void dInternalStepIsland_x2 (dxWorldProcessContext *context,
           for (int k=0; k<3; ++k) tmp1curr[4+k] += b->avel[k]*stepsizeRecip;
         }
       }
-
-#     ifdef COMPARE_METHODS
-      comparator.nextMatrix (c,m,1,0,"c");
-#     endif
 
       {
         // init rhs -- this erases 'c' as they reside in the same memory!!!
@@ -1044,11 +705,6 @@ void dInternalStepIsland_x2 (dxWorldProcessContext *context,
           ofsi += infom;
         }
       }
-
-#     ifdef COMPARE_METHODS
-      comparator.nextMatrix (rhs,m,1,0,"rhs");
-#     endif
-    
     } END_STATE_SAVE(context, tmp1state);
 
     dReal *lambda = context->AllocateArray<dReal> (m);
@@ -1061,10 +717,6 @@ void dInternalStepIsland_x2 (dxWorldProcessContext *context,
       dSolveLCP (context, m, A, lambda, rhs, NULL, nub, lo, hi, findex);
 
     } END_STATE_SAVE(context, lcpstate);
-
-#   ifdef COMPARE_METHODS
-    comparator.nextMatrix (lambda,m,1,0,"lambda");
-#   endif
 
     {
       IFTIMING(dTimerNow ("compute constraint force"));
@@ -1162,15 +814,6 @@ void dInternalStepIsland_x2 (dxWorldProcessContext *context,
     }
   }
 
-#ifdef COMPARE_METHODS
-  ALLOCA(dReal,tmp_vnew, nb*6*sizeof(dReal));
-  for (i=0; i<nb; ++i) {
-    for (j=0; j<3; ++j) tmp_vnew[i*6+j] = body[i]->lvel[j];
-    for (j=0; j<3; ++j) tmp_vnew[i*6+3+j] = body[i]->avel[j];
-  }
-  comparator.nextMatrix (tmp_vnew,nb*6,1,0,"vnew");
-#endif
-
   {
     IFTIMING(dTimerNow ("tidy up"));
 
@@ -1200,34 +843,7 @@ void dInternalStepIsland (dxWorldProcessContext *context,
                           dxWorld *world, dxBody * const *body, int nb,
                           dxJoint * const *joint, int nj, dReal stepsize)
 {
-#ifndef COMPARE_METHODS
   dInternalStepIsland_x2 (context,world,body,nb,joint,nj,stepsize);
-
-#endif
-
-#ifdef COMPARE_METHODS
-  int i;
-
-  // save body state
-  ALLOCA(dxBody,state,nb*sizeof(dxBody));
-
-  for (i=0; i<nb; i++) memcpy (state+i,body[i],sizeof(dxBody));
-
-  // take slow step
-  comparator.reset();
-  dInternalStepIsland_x1 (context,world,body,nb,joint,nj,stepsize);
-  comparator.end();
-
-  // restore state
-  for (i=0; i<nb; i++) memcpy (body[i],state+i,sizeof(dxBody));
-
-  // take fast step
-  dInternalStepIsland_x2 (context,world,body,nb,joint,nj,stepsize);
-  comparator.end();
-
-  //comparator.dump();
-  //_exit (1);
-#endif
 }
 
 size_t dxEstimateStepMemoryRequirements (dxBody * const *body, int nb, dxJoint * const *_joint, int _nj)
