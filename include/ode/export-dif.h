cd4d2ab0db01a6f8748798eeb63e86d5be724bc6
Oleh Derevenko <none@none>
2010-07-12 08:19:30 +0000

    Fixed: Missing extern "C" wrapper has been added to include/ode/export-dif.h (reported by Danny Price)

--- include/ode/export-dif.h
+++ include/ode/export-dif.h
@@ -26,7 +26,15 @@
 #include <ode/common.h>
 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 ODE_API void dWorldExportDIF (dWorldID w, FILE *file, const char *world_name);
 
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
