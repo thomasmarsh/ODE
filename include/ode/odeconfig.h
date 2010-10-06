8c441d430ce4de250854dcec0e16c58343c54fc7
Oleh Derevenko <oleh.derevenko@gmail.com>
2010-10-06 19:42:55 +0000

    Cosmetic: int8 type changed to be signed char rather than char in odeconfig.h

--- include/ode/odeconfig.h
+++ include/ode/odeconfig.h
@@ -41,14 +41,14 @@
   typedef unsigned int    uint32;
   typedef short           int16;
   typedef unsigned short  uint16;
-  typedef char            int8;
+  typedef signed char     int8;
   typedef unsigned char   uint8;
 #else
   typedef int             int32;
   typedef unsigned int    uint32;
   typedef short           int16;
   typedef unsigned short  uint16;
-  typedef char            int8;
+  typedef signed char     int8;
   typedef unsigned char   uint8;
 #endif
 
