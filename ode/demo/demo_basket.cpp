b3275b16885e2e541a78b5489e984700a4bb0e95
Bram Stolk <none@none>
2011-09-29 19:26:54 +0000

    Fix the contact merging for sphere/trimesh collisions.

--- ode/demo/demo_basket.cpp
+++ ode/demo/demo_basket.cpp
@@ -115,11 +115,7 @@ static void start()
 
 static void reset_ball(void)
 {
-  float sx=0.0f, sy=3.40f, sz=7.05;
-
-#if defined(_MSC_VER) && defined(dDOUBLE)
-  sy -= 0.01; // Cheat, to make it score under win32/double
-#endif
+  float sx=0.0f, sy=3.40f, sz=7.15;
 
   dQuaternion q;
   dQSetIdentity(q);
