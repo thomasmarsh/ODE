e461c8f00ec23c6166f01d7502e5ee219a73db60
Gideon Klompje <none@none>
2011-11-04 10:29:36 +0000

    refactored Python bindings tutorials 1 and 2 for clarity

--- bindings/python/demos/tutorial1.py
+++ bindings/python/demos/tutorial1.py
@@ -4,29 +4,46 @@
 
 # pyODE example 1: Getting started
 
+# modified by Gideon Klompje (removed literals and using
+# 'ode.Mass.setSphereTotal' instead of 'ode.Mass.setSphere')
+
 import ode
+
+# Simulation constants
+GRAVITY = (0, -9.81, 0)
+
+SPHERE_RADIUS = 0.05
+SPHERE_MASS = 1.0
+SPHERE_START_POS = (0, 2, 0)
+SPHERE_FORCE = (0, 200, 0) # Initial force to apply to the sphere
+
+TIME_STEP = 0.04
+TIME_STOP = 2.0 # When to stop the simulation
 
 # Create a world object
 world = ode.World()
-world.setGravity( (0,-9.81,0) )
+world.setGravity(GRAVITY)
 
-# Create a body inside the world
+# Create a spherical body inside the world
 body = ode.Body(world)
-M = ode.Mass()
-M.setSphere(2500.0, 0.05)
-M.mass = 1.0
-body.setMass(M)
+mass = ode.Mass()
+mass.setSphereTotal(SPHERE_MASS, SPHERE_RADIUS)
+body.setMass(mass)
 
-body.setPosition( (0,2,0) )
-body.addForce( (0,200,0) )
+body.setPosition(SPHERE_START_POS)
+body.addForce(SPHERE_FORCE)
 
 # Do the simulation...
-total_time = 0.0
-dt = 0.04
-while total_time<2.0:
-    x,y,z = body.getPosition()
-    u,v,w = body.getLinearVel()
-    print "%1.2fsec: pos=(%6.3f, %6.3f, %6.3f)  vel=(%6.3f, %6.3f, %6.3f)" % \
-          (total_time, x, y, z, u,v,w)
-    world.step(dt)
-    total_time+=dt
+if __name__ == "__main__":
+    total_time = 0.0
+    while total_time < TIME_STOP:
+        # output the body's position and velocity
+        x, y, z = body.getPosition()
+        u, v, w = body.getLinearVel()
+        print "%1.2fsec: pos=(%6.3f, %6.3f, %6.3f)  vel=(%6.3f, %6.3f, %6.3f)" % \
+            (total_time, x, y, z, u, v, w)
+
+        # advance the simulation
+        world.step(TIME_STEP)
+        total_time += TIME_STEP
+
