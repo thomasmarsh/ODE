cfeec1c2778884a1154e0839defa1d5034216ed0
Oleh Derevenko <oleh.derevenko@gmail.com>
2012-02-04 14:05:32 +0000

    Compilation errors fixed

--- ode/src/ode.cpp
+++ ode/src/ode.cpp
@@ -498,7 +498,7 @@ void dBodySetMass (dBodyID b, const dMass *mass)
   // Use dMassTranslate( mass, -mass->c[0], -mass->c[1], -mass->c[2] ) to correct it.
   dUASSERT( fabs( mass->c[0] ) <= dEpsilon &&
 			fabs( mass->c[1] ) <= dEpsilon &&
-			fabs( mass->c[2] ) <= dEpsilon, "The centre of mass must be at the origin." )
+			fabs( mass->c[2] ) <= dEpsilon, "The centre of mass must be at the origin." );
 
   memcpy (&b->mass,mass,sizeof(dMass));
   if (dInvertPDMatrix (b->mass.I,b->invI,3,NULL)==0) {
