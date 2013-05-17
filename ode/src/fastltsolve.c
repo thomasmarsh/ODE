0a29ebec871e55035378a5754743a06d7f92d2ef
Oleh Derevenko <none@none>
2013-05-17 22:57:45 +0000

    Merged: oleh_derevenko#1925: Cosmetic: Header inclusion fixed (might cause linking errors)

--- ode/src/fastltsolve.c
+++ ode/src/fastltsolve.c
@@ -22,8 +22,9 @@
 
 /* generated code, do not edit. */
 
-#include "ode/matrix.h"
+#include <ode/common.h>
 #include "config.h"
+#include "matrix.h"
 
 /* solve L^T * x=b, with b containing 1 right hand side.
  * L is an n*n lower triangular matrix with ones on the diagonal.
