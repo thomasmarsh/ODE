779ad8985c67c5abb6a1b816ec7759c972aac6c7
ppiastucki <none@none>
2016-09-17 14:05:07 +0200

    Add support for convex vs trimesh colision for OPCODE

--- ode/src/collision_convex_trimesh.cpp
+++ ode/src/collision_convex_trimesh.cpp
@@ -57,8 +57,32 @@ int dCollideConvexTrimesh( dxGeom *o1, dxGeom *o2, int flags, dContactGeom* cont
 #if dLIBCCD_ENABLED
 
 #if dTRIMESH_OPCODE
+    const dVector3& tPosition = *(const dVector3*)dGeomGetPosition(o2);
+    // Find convex OBB in trimesh coordinates
+    Point min(o1->aabb[0] - tPosition[0], o1->aabb[2] - tPosition[1], o1->aabb[4] - tPosition[2]);
+    Point max(o1->aabb[1] - tPosition[0], o1->aabb[3] - tPosition[1], o1->aabb[5] - tPosition[2]);
+    OBB obbConvex;
+    obbConvex.mCenter.Set(0.5f * (max + min));
+    obbConvex.mExtents.Set(0.5f * (max - min));
+    obbConvex.mRot.Set(1, 0, 0, 0, 1, 0, 0, 0, 1);
 
-
+    Matrix4x4 matrix;
+    const dMatrix3& tRotation = *(const dMatrix3*)dGeomGetRotation(o2);
+    const dVector3 ZeroVector3 = { REAL(0.0), };
+    MakeMatrix(ZeroVector3, tRotation, matrix);
+    OBBCollider collider;
+    collider.SetFirstContact(false);
+    collider.SetTemporalCoherence(false);
+    collider.SetPrimitiveTests(false);
+    OBBCache cache;
+    dxTriMesh *trimesh = (dxTriMesh *)o2;
+    if (collider.Collide(cache, obbConvex, trimesh->Data->BVTree, null, &matrix)) {
+        int triCount = collider.GetNbTouchedPrimitives();
+        if (triCount > 0) {
+            int* triangles = (int*)collider.GetTouchedPrimitives();
+            contactcount = dCollideConvexTrimeshTrianglesCCD(o1, o2, triangles, triCount, flags, contacts, skip);
+        }
+    }
 
 #elif dTRIMESH_GIMPACT
     dxTriMesh *trimesh = (dxTriMesh *)o2;
