633a724a5b43365cd75e205d58a53ac5c2358dbf
Bram Stolk <b.stolk@gmail.com>
2018-10-19 12:43:44 -0700

    Fix compile issue for double precision configurations.

--- ode/demo/demo_convex.cpp
+++ ode/demo/demo_convex.cpp
@@ -26,7 +26,7 @@
 
 #include <assert.h>
 #ifdef HAVE_UNISTD_H
-#include <unistd.h>
+#	include <unistd.h>
 #endif
 #include <ode/ode.h>
 #include <drawstuff/drawstuff.h>
@@ -34,8 +34,14 @@
 
 #include "halton235_geom.h"
 
+#ifdef dDOUBLE
+#	define dsDrawConvex dsDrawConvexD
+#	define dsDrawLine dsDrawLineD
+#endif
+
+
 #ifdef _MSC_VER
-#pragma warning(disable:4244 4305)  // for VC++, no precision loss complaints
+#	pragma warning(disable:4244 4305)  // for VC++, no precision loss complaints
 #endif
 
 
@@ -53,48 +59,47 @@ static dGeomID hgeom[ halton_numc ];
 static dJointGroupID contactgroup;
 
 static bool drawpos=false;
-static bool solidkernel=true;
+static bool solidkernel=false;
 
 
 // this is called by dSpaceCollide when two objects in space are
 // potentially colliding.
 
-static void nearCallback (void *data, dGeomID o1, dGeomID o2)
+static void nearCallback(void *data, dGeomID o1, dGeomID o2)
 {
-  assert(o1);
-  assert(o2);
+	assert(o1);
+	assert(o2);
+	if (dGeomIsSpace(o1) || dGeomIsSpace(o2))
+	{
+		// colliding a space with something
+		dSpaceCollide2(o1,o2,data,&nearCallback);
+		// Note we do not want to test intersections within a space,
+		// only between spaces.
+		return;
+	}
 
-  if (dGeomIsSpace(o1) || dGeomIsSpace(o2))
-  {
-    // colliding a space with something
-    dSpaceCollide2(o1,o2,data,&nearCallback);
-    // Note we do not want to test intersections within a space,
-    // only between spaces.
-    return;
-  }
-
-  const int N = 32;
-  dContact contact[N];
-  int n = dCollide (o1,o2,N,&(contact[0].geom),sizeof(dContact));
-  if (n > 0) 
-  {
-    for (int i=0; i<n; i++) 
-    {
-      contact[i].surface.slip1 = 0.7;
-      contact[i].surface.slip2 = 0.7;
-      contact[i].surface.mode = dContactSoftERP | dContactSoftCFM | dContactApprox1 | dContactSlip1 | dContactSlip2;
-      contact[i].surface.mu = 500.0; // was: dInfinity
-      contact[i].surface.soft_erp = 0.50;
-      contact[i].surface.soft_cfm = 0.03;
-      dJointID c = dJointCreateContact (world,contactgroup,&contact[i]);
-      dJointAttach
-      (
-        c,
-        dGeomGetBody(contact[i].geom.g1),
-        dGeomGetBody(contact[i].geom.g2)
-      );
-    }
-  }
+	const int N = 32;
+	dContact contact[N];
+	int n = dCollide (o1,o2,N,&(contact[0].geom),sizeof(dContact));
+	if (n > 0) 
+	{
+		for (int i=0; i<n; i++) 
+		{
+			contact[i].surface.slip1 = 0.7;
+			contact[i].surface.slip2 = 0.7;
+			contact[i].surface.mode = dContactSoftERP | dContactSoftCFM | dContactApprox1 | dContactSlip1 | dContactSlip2;
+			contact[i].surface.mu = 500.0; // was: dInfinity
+			contact[i].surface.soft_erp = 0.50;
+			contact[i].surface.soft_cfm = 0.03;
+			dJointID c = dJointCreateContact (world,contactgroup,&contact[i]);
+			dJointAttach
+			(
+				c,
+				dGeomGetBody(contact[i].geom.g1),
+				dGeomGetBody(contact[i].geom.g2)
+			);
+		}
+	}
 }
 
 
@@ -102,206 +107,201 @@ static void nearCallback (void *data, dGeomID o1, dGeomID o2)
 
 static void start()
 {
-  dAllocateODEDataForThread(dAllocateMaskAll);
-
-  static float xyz[3] = {-8,0,5};
-  static float hpr[3] = {0.0f,-29.5000f,0.0000f};
-  dsSetViewpoint (xyz,hpr);
-  fprintf(stderr,"Press SPACE to reset the simulation.\n");
+	dAllocateODEDataForThread(dAllocateMaskAll);
+	static float xyz[3] = {-8,0,5};
+	static float hpr[3] = {0.0f,-29.5000f,0.0000f};
+	dsSetViewpoint (xyz,hpr);
+	fprintf(stderr,"Press SPACE to reset the simulation.\n");
 }
 
 
 static void reset()
 {
-  dQuaternion q;
-  dQSetIdentity(q);
-  dBodySetPosition(mbody,0,0,0+H);
-  dBodySetQuaternion(mbody, q);
-  dBodySetLinearVel(mbody, 0,0,0);
-  dBodySetAngularVel(mbody, 0,0,0);
-  dBodyEnable(mbody);
-  for ( int i=0; i<halton_numc; ++i )
-  {
-    dBodyID body = hbody[i];
-    if ( !body ) continue;
-    dBodySetPosition(body, halton_pos[i][0], halton_pos[i][1], halton_pos[i][2]+H);
-    dBodySetQuaternion(body, q);
-    dBodySetLinearVel(body, 0,0,0);
-    dBodySetAngularVel(body, 0,0,0);
-    dBodyEnable(body);
-  }
+	dQuaternion q;
+	dQSetIdentity(q);
+	dBodySetPosition(mbody,0,0,0+H);
+	dBodySetQuaternion(mbody, q);
+	dBodySetLinearVel(mbody, 0,0,0);
+	dBodySetAngularVel(mbody, 0,0,0);
+	dBodyEnable(mbody);
+	for ( int i=0; i<halton_numc; ++i )
+	{
+		dBodyID body = hbody[i];
+		if ( !body ) continue;
+		dBodySetPosition(body, halton_pos[i][0], halton_pos[i][1], halton_pos[i][2]+H);
+		dBodySetQuaternion(body, q);
+		dBodySetLinearVel(body, 0,0,0);
+		dBodySetAngularVel(body, 0,0,0);
+		dBodyEnable(body);
+	}
 }
 
 
 // called when a key pressed
 
-static void command (int cmd)
+static void command(int cmd)
 {
-  switch (cmd) 
-  {
-    case ' ':
-      reset();
-      break;
-    default:
-      break;
-  }
+	switch (cmd) 
+	{
+		case ' ':
+			reset();
+			break;
+		default:
+			break;
+	}
 }
 
 
-// simulation loop
 
-static void simLoop (int pause)
+static void simLoop(int pause)
 {
-  double simstep = 1/240.0;
-  double dt = dsElapsedTime();
+	double simstep = 1/240.0;
+	double dt = dsElapsedTime();
 
-  int nrofsteps = (int) ceilf(dt/simstep);
-  nrofsteps = nrofsteps > 8 ? 8 : nrofsteps;
+	int nrofsteps = (int) ceilf(dt/simstep);
+	nrofsteps = nrofsteps > 8 ? 8 : nrofsteps;
 
-  for (int i=0; i<nrofsteps && !pause; i++)
-  {
-    dSpaceCollide (space,0,&nearCallback);
-    dWorldQuickStep (world, simstep);
-    dJointGroupEmpty (contactgroup);
-  }
+	for (int i=0; i<nrofsteps && !pause; i++)
+	{
+		dSpaceCollide (space,0,&nearCallback);
+		dWorldQuickStep (world, simstep);
+		dJointGroupEmpty (contactgroup);
+	}
 
-  dsSetColor (1,1,1);
-  // Draw the convex objects.
-  for ( int i=0; i<halton_numc; ++i )
-  {
-    dGeomID geom = hgeom[i];
-    dBodyID body = dGeomGetBody(geom);
-    //const dReal *pos = dBodyGetPosition(body);
-    //const dReal *rot = dBodyGetRotation(body);
-    const dReal *pos = dGeomGetPosition(geom);
-    const dReal *rot = dGeomGetRotation(geom);
-    dsDrawConvex
-    (
-      pos, rot,
-      halton_planes[i],
-      halton_numf[i],
-      halton_verts[i],
-      halton_numv[i],
-      halton_faces[i]
-    );
-  }
+	dsSetColor (1,1,1);
+	// Draw the convex objects.
+	for ( int i=0; i<halton_numc; ++i )
+	{
+		dGeomID geom = hgeom[i];
+		dBodyID body = dGeomGetBody(geom);
+		//const dReal *pos = dBodyGetPosition(body);
+		//const dReal *rot = dBodyGetRotation(body);
+		const dReal *pos = dGeomGetPosition(geom);
+		const dReal *rot = dGeomGetRotation(geom);
+		dsDrawConvex
+		(
+			pos, rot,
+			halton_planes[i],
+			halton_numf[i],
+			halton_verts[i],
+			halton_numv[i],
+			halton_faces[i]
+		);
+	}
 
-  if (drawpos)
-  {
-    dsSetColor(1,0,0.2);
-    dsSetTexture(DS_NONE);
-    const dReal l = 0.35;
-    for ( int i=0; i<halton_numc; ++i )
-    {
-      dBodyID body = hbody[i];
-      const dReal *pos = dBodyGetPosition(body);
-      dReal x0[3] = { pos[0]-l, pos[1], pos[2] };
-      dReal x1[3] = { pos[0]+l, pos[1], pos[2] };
-      dReal y0[3] = { pos[0], pos[1]-l, pos[2] };
-      dReal y1[3] = { pos[0], pos[1]+l, pos[2] };
-      dReal z0[3] = { pos[0], pos[1], pos[2]-l };
-      dReal z1[3] = { pos[0], pos[1], pos[2]+l };
-
-      dsDrawLine(x0,x1);
-      dsDrawLine(y0,y1);
-      dsDrawLine(z0,z1);
-    }
-  }
+	if (drawpos)
+	{
+		dsSetColor(1,0,0.2);
+		dsSetTexture(DS_NONE);
+		const dReal l = 0.35;
+		for ( int i=0; i<halton_numc; ++i )
+		{
+			dBodyID body = hbody[i];
+			const dReal *pos = dBodyGetPosition(body);
+			dReal x0[3] = { pos[0]-l, pos[1], pos[2] };
+			dReal x1[3] = { pos[0]+l, pos[1], pos[2] };
+			dReal y0[3] = { pos[0], pos[1]-l, pos[2] };
+			dReal y1[3] = { pos[0], pos[1]+l, pos[2] };
+			dReal z0[3] = { pos[0], pos[1], pos[2]-l };
+			dReal z1[3] = { pos[0], pos[1], pos[2]+l };
+			dsDrawLine(x0,x1);
+			dsDrawLine(y0,y1);
+			dsDrawLine(z0,z1);
+		}
+	}
 }
 
 
 int main (int argc, char **argv)
 {
-  dMass m;
+	dMass m;
 
-  // setup pointers to drawstuff callback functions
-  dsFunctions fn;
-  fn.version = DS_VERSION;
-  fn.start = &start;
-  fn.step = &simLoop;
-  fn.command = &command;
-  fn.stop = 0;
-  fn.path_to_textures = DRAWSTUFF_TEXTURE_PATH;
+	// setup pointers to drawstuff callback functions
+	dsFunctions fn;
+	fn.version = DS_VERSION;
+	fn.start = &start;
+	fn.step = &simLoop;
+	fn.command = &command;
+	fn.stop = 0;
+	fn.path_to_textures = DRAWSTUFF_TEXTURE_PATH;
 
-  // create world
-  dInitODE2(0);
-  world = dWorldCreate();
-  space = dHashSpaceCreate (0);
-  dHashSpaceSetLevels(space, -3, 5);
-  dCreatePlane(space,0,0,1,0);	// Add a ground plane.
+	// create world
+	dInitODE2(0);
+	world = dWorldCreate();
+	space = dHashSpaceCreate (0);
+	dHashSpaceSetLevels(space, -3, 5);
+	dCreatePlane(space,0,0,1,0);	// Add a ground plane.
 
-  contactgroup = dJointGroupCreate (0);
-  dWorldSetGravity(world,0,0,-9.8);
-  dWorldSetQuickStepNumIterations(world, 32);
-  dWorldSetContactMaxCorrectingVel(world, 40);
-  dWorldSetMaxAngularSpeed(world, 62.8);
-  dWorldSetERP(world, 0.7);
-  dWorldSetQuickStepW(world, 0.75); // For increased stability.
+	contactgroup = dJointGroupCreate (0);
+	dWorldSetGravity(world,0,0,-9.8);
+	dWorldSetQuickStepNumIterations(world, 32);
+	dWorldSetContactMaxCorrectingVel(world, 40);
+	dWorldSetMaxAngularSpeed(world, 62.8);
+	dWorldSetERP(world, 0.7);
+	dWorldSetQuickStepW(world, 0.75); // For increased stability.
 
-  dWorldSetAutoDisableFlag( world, true );
-  dWorldSetAutoDisableLinearThreshold( world, 0.01 );
-  dWorldSetAutoDisableAngularThreshold( world, 0.03 );
-  dWorldSetAutoDisableTime( world, 0.15f );
+	dWorldSetAutoDisableFlag( world, true );
+	dWorldSetAutoDisableLinearThreshold( world, 0.01 );
+	dWorldSetAutoDisableAngularThreshold( world, 0.03 );
+	dWorldSetAutoDisableTime( world, 0.15f );
 
-  const float kernelrad = 0.7;
+	const float kernelrad = 0.7;
 
-  mbody = dBodyCreate(world);
-  dBodySetPosition(mbody, 0,0,0+H);
-  dMassSetSphere( &m, 5, kernelrad );
-  dBodySetMass( mbody, &m );
+	mbody = dBodyCreate(world);
+	dBodySetPosition(mbody, 0,0,0+H);
+	dMassSetSphere( &m, 5, kernelrad );
+	dBodySetMass( mbody, &m );
 
-  for (int i=0; i<halton_numc; ++i )
-  {
-    dGeomID geom = dCreateConvex
-    (
-      space,
-      halton_planes[i],
-      halton_numf[i],
-      halton_verts[i],
-      halton_numv[i],
-      halton_faces[i]
-    );
-    hgeom[i] = geom;
-    dReal x = halton_pos[i][0];
-    dReal y = halton_pos[i][1];
-    dReal z = halton_pos[i][2];
-    dReal dsqr = x*x + y*y + z*z;
+	for (int i=0; i<halton_numc; ++i )
+	{
+		dGeomID geom = dCreateConvex
+		(
+			space,
+			halton_planes[i],
+			halton_numf[i],
+			halton_verts[i],
+			halton_numv[i],
+			halton_faces[i]
+		);
+		hgeom[i] = geom;
+		const dReal x = halton_pos[i][0];
+		const dReal y = halton_pos[i][1];
+		const dReal z = halton_pos[i][2];
+		const dReal dsqr = x*x + y*y + z*z;
 
-    if ( dsqr < kernelrad*kernelrad && solidkernel )
-    {
-      dGeomSetBody(geom, mbody);
-      dGeomSetOffsetPosition(geom, x,y,z);
-    }
-    else
-    {
-      dBodyID body = dBodyCreate(world);
-      hbody[i] = body;
-      dBodySetPosition(body, x,y,z+H);
-      dReal volu = halton_volu[i];
-      dReal rad = pow( volu * 3 / (4*M_PI), (1/3.0) );
-      dMassSetSphere( &m,5,rad );
-      dBodySetMass( body,&m );
+		if ( dsqr < kernelrad*kernelrad && solidkernel )
+		{
+			dGeomSetBody(geom, mbody);
+			dGeomSetOffsetPosition(geom, x,y,z);
+		}
+		else
+		{
+			dBodyID body = dBodyCreate(world);
+			hbody[i] = body;
+			dBodySetPosition(body, x,y,z+H);
+			dReal volu = halton_volu[i];
+			dReal rad = pow( volu * 3 / (4*M_PI), (1/3.0) );
+			dMassSetSphere( &m,5,rad );
+			dBodySetMass( body,&m );
 #if 1
-      dBodySetLinearDamping (body, 0.0005);
-      dBodySetAngularDamping(body, 0.0300);
+			dBodySetLinearDamping (body, 0.0005);
+			dBodySetAngularDamping(body, 0.0300);
 #endif
-      dGeomSetBody(geom,body);
-    }
-  }
+			dGeomSetBody(geom,body);
+		}
+	}
 
-  // run simulation
-  // const int w=352;
-  // const int h=288;
-  const int w=1280;
-  const int h=720;
-  dsSimulationLoop (argc,argv,w,h,&fn);
+	// run simulation
+	const int w=1280;
+	const int h=720;
+	dsSimulationLoop (argc,argv,w,h,&fn);
 
-  dJointGroupEmpty (contactgroup);
-  dJointGroupDestroy (contactgroup);
-  dSpaceDestroy (space);
-  dWorldDestroy (world);
-  dCloseODE();
-  return 0;
+	dJointGroupEmpty (contactgroup);
+	dJointGroupDestroy (contactgroup);
+	dSpaceDestroy (space);
+	dWorldDestroy (world);
+	dCloseODE();
+	return 0;
 }
 
 
