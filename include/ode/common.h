f58a19a8ee2dcdd8d7d4102b3b6109c75d964839
Oleh Derevenko <oleh.derevenko@gmail.com>
2019-01-30 01:01:20 +0200

    Changed: dMax()/dMin() functions have been implemented via the C Library functions rather than via direct expression

--- include/ode/common.h
+++ include/ode/common.h
@@ -294,6 +294,8 @@ typedef dReal dQuaternion[dQUE__MAX];
 #define dCeil(x) ceilf(x)			/* ceil */
 #define dCopySign(a,b) _ode_copysignf(a, b) /* copy value sign */
 #define dNextAfter(x, y) _ode_nextafterf(x, y) /* next value after */
+#define dMax(a, b) fmaxf(a, b)
+#define dMin(a, b) fminf(a, b)
 
 #ifdef HAVE___ISNANF
 #define dIsNan(x) (__isnanf(x))
@@ -331,6 +333,8 @@ typedef dReal dQuaternion[dQUE__MAX];
 #define dCeil(x) ceil(x)
 #define dCopySign(a,b) _ode_copysign(a, b)
 #define dNextAfter(x, y) _ode_nextafter(x, y)
+#define dMax(a, b) fmax(a, b)
+#define dMin(a, b) fmin(a, b)
 
 #ifdef HAVE___ISNAN
 #define dIsNan(x) (__isnan(x))
@@ -345,9 +349,6 @@ typedef dReal dQuaternion[dQUE__MAX];
 #else
 #error You must #define dSINGLE or dDOUBLE
 #endif
-
-ODE_PURE_INLINE dReal dMin(dReal x, dReal y) { return x <= y ? x : y; }
-ODE_PURE_INLINE dReal dMax(dReal x, dReal y) { return x <= y ? y : x; }
 
 
 /* internal object types (all prefixed with `dx') */
